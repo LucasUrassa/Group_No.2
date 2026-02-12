9cc39274c2e86dd99f060ba428684503dc4a1274
Flywizz <lucascostantine2004@gmail.com>
2026-02-12 14:10:18 +0300

    Person.java
    Added Person.java

---
+++ Person.java
@@ -0,0 +1,40 @@
+/**
+ * Write a description of class MobilePhone here.
+ *
+ * @author (24BIA023)
+ * @version (12/5/2026)
+ */
+public class Person {
+    private String name;
+    private String address;
+    
+    //constructor
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    //Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString(){
+        return "Person[name=" + name + ", address=" + address + "]";
+    }
+
+
+
+}
